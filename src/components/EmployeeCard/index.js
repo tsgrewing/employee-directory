6ecb0be4399b599f7aa1d4f671b34ea2e3988706
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-09-22 12:43:05 -0500

    setting up cardlist

---
+++ src/components/EmployeeCard/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style.css";
+
+function EmployeeCard(props) {
+  return (
+    <div className="card col-sm-3">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Phone:</strong> {props.phone}
+          </li>
+          <li>
+            <strong>Email:</strong> {props.email}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default EmployeeCard;
