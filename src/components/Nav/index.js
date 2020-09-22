e274ec8f41b1dbe80c4410cc95540ab18ed06ec7
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-09-22 12:51:14 -0500

    setting up nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./style.css";
+
+function Nav(props) {
+    return (
+        <nav>
+            <form className="search">
+                <label htmlFor="employeeSearch">Search:</label>
+                <input
+                    value={props.search}
+                    onChange={props.handleInputChange}
+                    name="employeeSearch"
+                    className="form-control"
+                    placeholder="Search for Employee"
+                    id="search"
+                />
+            </form>
+        </nav> 
+    )
+}
+
+export default Nav;
