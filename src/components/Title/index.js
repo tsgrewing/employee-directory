f5017dae6e70541ab5962a8d58d3b096f40a8905
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-09-21 23:25:22 -0500

    updating title bar

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./style.css";
 
-function Title(props) {
-  return <h1 className="title">{props.children}</h1>;
+function Title() {
+  return (
+  <h1 className="title">Employee Directory</h1>
+  );
 }
 
 export default Title;
