533a140bd25acea54c0c8e3539da3fc61b3cd7da
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-09-21 19:50:53 -0500

    updating app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./employees.json";
 
-class App extends React.Component() {
+class App extends Component {
+  // Setting this.state.employees to the employees json array
   state = {
-    employees = {}
+    employees
   };
 
-  componentDidMount() {
-    $.ajax({
-      url: 'https://randomuser.me/api/?results=20',
-      dataType: 'json',
-      success: function(data) {
-        console.log(data);
-      }
-    });
+  removeFriend = id => {
+    // Filter this.state.employees for employees with an id not equal to the id being removed
+    const employees = this.state.employees.filter(employee => employee.id !== id);
+    // Set this.state.employees equal to the new employees array
+    this.setState({ employees });
   };
 
+  // Map over this.state.employees and render a EmployeeCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Employee List</Title>
+        {this.state.employees.map(employee => (
+          <EmployeeCard
+            id={employee.id}
+            key={employee.id}
+            email={employee.email}
+            phone={employee.phone}
+            name={employee.name}
+            image={employee.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
+
 export default App;
---
+++ src/utils/API.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const BASEURL = "https://randomuser.me/api/?results=20";
+
+
+export default {
+  search: function() {
+    return axios.get(BASEURL);
+  }
+};
