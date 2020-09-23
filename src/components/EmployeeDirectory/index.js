b2e3690ba2fac059457e95587aa8169bb2f9a197
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-09-23 00:00:36 -0500

    fixing styles

--- src/components/EmployeeDirectory/index.js
+++ src/components/EmployeeDirectory/index.js
@@ -22,55 +22,55 @@ class EmployeeDirectory extends Component {
     };
 
     // Combine email and name sorting to a single sortAlphabetically function and use data att to determine what to sort by
-    sortByName = (target) => {
-        const searchedList = this.state.filteredEmployees;
+    sortByName = () => {
+        const empList = this.state.filteredEmployees;
         if (this.state.order === "ascend") {
-            const sortedList = searchedList.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1)
+            const sortedList = empList.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1)
             this.setState({
-                searchedList: sortedList,
+                employees: sortedList,
                 order: "descend"
             })
         }
         else {
-            const sortedList = searchedList.sort((a, b) => (a.name.first > b.name.first) ? -1 : 1)
+            const sortedList = empList.sort((a, b) => (a.name.first > b.name.first) ? -1 : 1)
             this.setState({
-                searchedList: sortedList,
+                employees: sortedList,
                 order: "ascend"            
             })
         };
     };
 
     sortByEmail = () => {
-        const searchedList = this.state.filteredEmployees;
+        const empList = this.state.filteredEmployees;
         if (this.state.order === "ascend") {
-            const sortedList = searchedList.sort((a, b) => (a.email > b.email) ? 1 : -1)
+            const sortedList = empList.sort((a, b) => (a.email > b.email) ? 1 : -1)
             this.setState({
-                searchedList: sortedList,
+                employees: sortedList,
                 order: "descend"
             })
         }
         else {
-            const sortedList = searchedList.sort((a, b) => (a.email > b.email) ? -1 : 1)
+            const sortedList = empList.sort((a, b) => (a.email > b.email) ? -1 : 1)
             this.setState({
-                searchedList: sortedList,
+                employees: sortedList,
                 order: "ascend"            
             })
         };
     };
     
     sortByNumber = () => {
-        const searchedList = this.state.filteredEmployees;
+        const empList = this.state.filteredEmployees;
         if (this.state.order === "ascend") {
-            const sortedList = searchedList.sort((a, b) => (a.phone > b.phone) ? 1 : -1)
+            const sortedList = empList.sort((a, b) => (a.phone > b.phone) ? 1 : -1)
             this.setState({
-                searchedList: sortedList,
+                empList: sortedList,
                 order: "descend"
             })
         }
         else {
-            const sortedList = searchedList.sort((a, b) => (a.phone > b.phone) ? -1 : 1)
+            const sortedList = empList.sort((a, b) => (a.phone > b.phone) ? -1 : 1)
             this.setState({
-                searchedList: sortedList,
+                empList: sortedList,
                 order: "ascend"            
             })
         };
@@ -81,6 +81,9 @@ class EmployeeDirectory extends Component {
         const employees = this.state.employees;
         const searchedName = evt.target.value;
         const filteredEmployees = employees.filter(emp => emp.name.first.toLowerCase().indexOf(searchedName.toLowerCase()) > -1)
+        if (!filteredEmployees) {
+            return this.setState
+        }
         this.setState({filteredEmployees: filteredEmployees})
     }
 
